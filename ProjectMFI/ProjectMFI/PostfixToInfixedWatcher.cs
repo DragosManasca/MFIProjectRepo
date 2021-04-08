9a79127096c25975d2cb04124e1edf93ef7f6ec6
DragosManasca <dragosmanasca@gmail.com>
2021-04-08 19:52:52 +0300

    some some nugets

--- ProjectMFI/ProjectMFI/PostfixToInfixedWatcher.cs
+++ ProjectMFI/ProjectMFI/PostfixToInfixedWatcher.cs
@@ -1,11 +1,8 @@
 ﻿using PostfixToInfixed;
-using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProjectMFI
